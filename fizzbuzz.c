138b4214d525d79f9cdf3703891dafb74c431273
VaishaliBadade <badadevaishu14@gmail.com>
2016-09-06 09:53:40 +0530

    Create fizzbuzz.c

---
+++ fizzbuzz.c
@@ -0,0 +1,23 @@
+#include<conio.h>
+#include<stdio.h>
+void main()(
+{
+int i;
+for(i=1;i<=100;i++)
+{
+if(i%3==0&&i%5==0)
+{
+printf("FizzBuzz);
+}
+elseif(i%3==0)
+{
+printf("Fizz");
+}
+elseif(i%5==0)
+{
+printf("Buzz");
+}
+else
+printf("i",%d);
+}
+}
